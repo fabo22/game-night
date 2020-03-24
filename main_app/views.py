5ece140461f580ca9126a6e21dee4d852242f43a
fabo <lopezfabrizio@ymail.com>
2020-03-24 12:00:22 -0500

    implement edit and delete on events

--- main_app/views.py
+++ main_app/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import GameGroup, Attending, Application, Event, Genre, Photo
-from .forms import EventForm
+from .forms import AddEventForm, EditEventForm
 # Create your views here.
 
 class GroupList(LoginRequiredMixin, ListView):
@@ -23,17 +23,46 @@ class GroupCreate(LoginRequiredMixin, CreateView):
 @login_required
 def groups_detail(request, group_id):
     gamegroup = GameGroup.objects.get(id=group_id)
-    event_form = EventForm()
+    edit_event_form = EditEventForm()
+    add_event_form = AddEventForm()
     return render(request, 'groups/detail.html', {
         'gamegroup': gamegroup,
-        'event_form': event_form,
+        'add_event_form': add_event_form,
+        'edit_event_form': edit_event_form,
         # Attending pass
     })
+
+@login_required
+def delete_event(request, event_id):
+  event = Event.objects.get(id=event_id)
+  group_id = event.group.id
+  if event.created_by == request.user:
+    event.delete()
+    return redirect('groups_detail', group_id)
+  else:
+    return redirect('groups_detail', group_id)
   
+def update_event(request, event_id):
+  event = Event.objects.get(id = event_id)
+  group_id = event.group.id
+  if event.created_by == request.user:
+    event.game_description=request.POST.get('game_description')
+    event.date=request.POST.get('date')
+    event.time=request.POST.get('time')
+    event.game=request.POST.get('game')
+    event.limit=request.POST.get('limit')
+    event.address=request.POST.get('address')
+    event.save()
+  else:
+    return redirect('groups_detail', group_id)
+  return redirect('groups_detail', group_id)
+
+
 @login_required
 def add_event(request, group_id):
-  form = EventForm(request.POST)
-  # print('Form Errors: ', form.errors)
+  # gamegroup = GameGroup.objects.get(id=group_id)
+  # if request.user in gamegroup.users:
+  form = AddEventForm(request.POST)
   print('******Form Data: ', form)
   if form.is_valid():
     print('********form valid*********')
